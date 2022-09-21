6da9052a5aca8bdd7e30e337d41fdbc034af6fb3
unknown <cmejia106@gmail.com>
2022-09-20 21:53:18 -0500

    Entiedades primera versión

---
+++ src/main/java/com/mireddesalud/MiRedSalud/backend/entities/Familiares.java
@@ -0,0 +1,48 @@
+package com.mireddesalud.MiRedSalud.backend.entities;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Familiares {
+    @Id
+    @Column(nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int edad;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private List<Enfermedades> enfermedades;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = true)
+    private List<TipoParendesco> parentesco;
+
+    
+}
---
+++ src/main/java/com/mireddesalud/MiRedSalud/backend/entities/Usuario.java
@@ -0,0 +1,56 @@
+package com.mireddesalud.MiRedSalud.backend.entities;
+
+import com.mireddesalud.MiRedSalud.backend.entities.Enfermedades;
+import com.mireddesalud.MiRedSalud.backend.entities.TipoParendesco;
+import com.mireddesalud.MiRedSalud.backend.entities.Familiares;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+    
+    @Id
+    @Column(nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int edad;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private List<Enfermedades> enfermedades;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = true)
+    private List<TipoParendesco> parentesco;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private List<Familiares> familiar;
+
+}
