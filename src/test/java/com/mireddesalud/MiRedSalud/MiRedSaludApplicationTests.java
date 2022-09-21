056c9f08acb19897ce709facec7aa3177833cf28
unknown <cmejia106@gmail.com>
2022-09-20 21:16:04 -0500

    primer commit con el proyecto sprint

---
+++ src/test/java/com/mireddesalud/MiRedSalud/MiRedSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mireddesalud.MiRedSalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiRedSaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
